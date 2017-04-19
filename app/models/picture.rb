61ee86b205fd099cdac60047dd261376c511ba7a
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-19 16:08:28 -0400

    add validations

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,9 @@
 class Picture < ApplicationRecord
+  validates :artist, presence: true
+
+  validates :title, length { in: 3..20 }
+
+  validates :url, presence: true
+  validates :url, uniqueness: true
+
 end
